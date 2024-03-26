69fbb7d4e11ee8080a3fe6620c883c3f0584e474
Samir Rodas <serodas9710@gmail.com>
2024-03-26 17:24:40 +0000

    Add books table

--- etc/databases/librarify.sql
+++ etc/databases/librarify.sql
@@ -9,3 +9,13 @@ CREATE TABLE `categories` (
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `books` (
+  `id` char(36) not null,
+  `title` varchar(255) not null,
+  `description` varchar(255) not null,
+  `score` INT NOT NULL,
+  `authors` JSON NOT NULL,
+  `categories` JSON NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
